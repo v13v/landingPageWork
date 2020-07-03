cc51fb816a5f6f5b45e963feef4934dd3bcb4a5d
Vadim Moroz <vmoroz13@yandex.ru>
2020-07-03 11:18:33 +0300

    clear setInterval

--- new18.6/main.js
+++ new18.6/main.js
@@ -1,55 +1,61 @@
-'use strict';
 const hi = document.querySelector('.hi'),
-day = document.querySelector('.day'),
-date = document.querySelector('.date'),
-newYear = document.querySelector('.new_year');
+    day = document.querySelector('.day'),
+    date = document.querySelector('.date'),
+    newYear = document.querySelector('.new_year');
 let message = '';
 const dateTime = new Date();
 let hour = dateTime.getHours();
- if(hour >= 5 && hour < 11){
+if (hour >= 5 && hour < 11) {
     message = "Доброе утро";
- }else if(hour >= 11 && hour < 18){
+} else if (hour >= 11 && hour < 18) {
     message = "Добрый день";
- }else if(hour >= 18 && hour < 23){
-    message = "Добрый вечер"; 
- }else if(hour >= 23 && hour < 5){
+} else if (hour >= 18 && hour < 24) {
+    message = "Добрый вечер";
+} else if (hour >= 0 && hour < 5) {
     message = "Доброй ночи";
- } 
- hi.innerHTML = message;//
+}
+hi.innerHTML = message;//
 
- let weekday = new Array("Воскресенье","Понедельник","Вторник",
-"Среда","Четверг","Пятница","Суббота");
+const weekday = new Array("Воскресенье", "Понедельник", "Вторник",
+    "Среда", "Четверг", "Пятница", "Суббота");
 day.innerHTML = `Сегодня: ${weekday[dateTime.getDay()]}`;//
-
-hour = dateTime.getHours();
-let minutes = dateTime.getMinutes(),
-seconds = dateTime.getSeconds();
-let ampm = hour >= 12 ? 'PM' : 'AM';
+function appNull(time) {
+    if (time > 0 && time < 10) {
+        return '0' + time;
+    } else {
+        return time;
+    }
+}
+hour = appNull(dateTime.getHours());
+const minutes = appNull(dateTime.getMinutes()),
+    seconds = appNull(dateTime.getSeconds());
+const ampm = hour >= 12 ? 'PM' : 'AM';
 
 date.innerHTML = `Текущее время: ${hour}:${minutes}:${seconds} ${ampm}`;
 
 const nextDate = new Date("January 1, 2021"),
-//колличество милисекунд в одном дне
-msOneDay = 24*60*60*1000;
+    //колличество милисекунд в одном дне
+    msOneDay = 24 * 60 * 60 * 1000;
 // колличество дней
-let daysNumbers = Math.round((nextDate.getTime() - dateTime.getTime())/msOneDay);
+const daysNumbers = Math.round((nextDate.getTime() - dateTime.getTime()) / msOneDay);
 //определяем окончание
- let dayname = "";
-  let ds = "" + daysNumbers;
-  //Вырезаем последнею цифру
- let dd = parseInt(ds.substr(ds.length-1));
- //Определяем правильность написания
- if(daysNumbers > 4 && daysNumbers < 21 ){
+let dayname = "";
+const ds = "" + daysNumbers;
+//Вырезаем последнею цифру
+const dd = parseInt(ds.substr(ds.length - 1));
+//Определяем правильность написания
+if (daysNumbers > 4 && daysNumbers < 21) {
     dayname = " дней";
- } else if(dd === 1){
-    dayname=" день";
- } else if(dd===2||dd===3||dd===4){
-    dayname=" дня";
- } else{
-    dayname=" дней";
- }
- newYear.innerHTML = `
+} else if (dd === 1) {
+    dayname = " день";
+} else if (dd === 2 || dd === 3 || dd === 4) {
+    dayname = " дня";
+} else {
+    dayname = " дней";
+}
+newYear.innerHTML = `
  До нового года осталось ${daysNumbers} ${dayname}
- `; 
- 
-  
+ `;
+
+
+
--- script/script.js
+++ script/script.js
@@ -1,53 +1,54 @@
-window.addEventListener('DOMContentLoaded',function(){
-  'use strict';
-  // Timer
-  function countTimer(deadline){
-    let
-    timerHours = document.querySelector('#timer-hours'),
-    timerMinutes = document.querySelector('#timer-minutes'), 
-    timerSeconds = document.querySelector('#timer-seconds');
+window.addEventListener('DOMContentLoaded', () => {
 
-    function getTimeRemaining(){
-      let dateStop = new Date(deadline).getTime(),
-      dateNow = new Date().getTime(),
-      timeRemaining = (dateStop - dateNow) / 1000,
-      seconds = Math.floor(timeRemaining % 60),
-      minutes = Math.floor((timeRemaining / 60) % 60),
-      hours = Math.floor((timeRemaining / 60 / 60) );
-     return {
-        hours,
-        minutes,
-        seconds,
-        timeRemaining
-     };
+    // Timer
+    function countTimer(deadline) {
+        const
+            timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor((timeRemaining / 60 / 60));
+            return {
+                hours,
+                minutes,
+                seconds,
+                timeRemaining
+            };
+        }
+        function appDateClock() {
+            const timer = getTimeRemaining();
+
+            function appNull(time) {
+                if (time > 0 && time < 10) {
+                    return '0' + time;
+                } else {
+                    return time;
+                }
+            }
+
+            timerHours.textContent = appNull(timer.hours);
+            timerMinutes.textContent = appNull(timer.minutes);
+            timerSeconds.textContent = appNull(timer.seconds);
+
+            if (timer.timeRemaining > 0) {
+                setInterval(appDateClock, 1000);
+                clearInterval(setInterval(appDateClock, 1000));
+                //setTimeout(appDateClock, 1000);
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        }
+        appDateClock();
     }
-   function appDateClock(){
-     let timer = getTimeRemaining();
-
-     function appNull(time){
-      if (time > 0 && time < 10) { 
-        return '0' + time;
-      } else {
-        return time;
-      }
-    }
-
-     timerHours.textContent = appNull(timer.hours);
-     timerMinutes.textContent = appNull(timer.minutes);
-     timerSeconds.textContent = appNull(timer.seconds);
-
-     if(timer.timeRemaining > 0){
-      setInterval(appDateClock, 1000);
-     //setTimeout(appDateClock, 1000);
-     }else{
-      timerHours.textContent = '00';
-      timerMinutes.textContent = '00';
-      timerSeconds.textContent = '00';
-     }
-   }
-     
-   appDateClock();
-  }
-  //setInterval(countTimer, 1000, '4,july , 2020')
-  countTimer('03, july, 2020');
+    //setInterval(countTimer, 1000, '4,july , 2020');
+    countTimer('04, july, 2020');
 });
+
